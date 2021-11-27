b3dff94b131b81141c958b055581095abeb501ed
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2021-11-27 18:02:49 +0200

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,8 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>Roma</div>
+    <div>
+      <List />
+      <div className='App'>Roma</div>
+    </div>
   );
+}
+
+let List = () => {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
 }
 
 export default App;
