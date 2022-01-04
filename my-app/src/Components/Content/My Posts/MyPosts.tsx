78cc0de0547ee7ce58aeb10d75eeef1d091d578c
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-01-04 09:23:07 +0200

    Update MyPosts.tsx

--- my-app/src/Components/Content/My Posts/MyPosts.tsx
+++ my-app/src/Components/Content/My Posts/MyPosts.tsx
@@ -5,16 +5,28 @@ import {contentPageType, messagesDataType} from "../../Redax/redax";
 
 type myPostsType = {
     messages: messagesDataType[]
+    addPost: (postMessage: string) => void
 }
+
 export function MyPosts(props: myPostsType) {
-    let posts = props.messages.map((i) => <Post message={i.message} likesCount={i.likesCount}/>)
+    let posts = props.messages.map((i) => <Post key={i.id} message={i.message} likesCount={i.likesCount}/>)
+
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    function addPosts() {
+        if (newPostElement.current) {
+            let text = newPostElement.current?.value
+            props.addPost(text)
+        }
+    }
+
     return (
         <div className={classes.content}>
             <div>
                 MyPosts
                 <div>
-                    <textarea></textarea>
-                    <button>Add post</button>
+                    <textarea ref={newPostElement}></textarea>
+                    <button onClick={addPosts}>Add post</button>
                     <button>Remove post</button>
                     {posts}
                 </div>
