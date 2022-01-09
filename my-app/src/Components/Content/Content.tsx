23289c5f88816f8ae6320ed06782164a70b0bd27
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-01-09 16:42:12 +0200

    Update Content.tsx

--- my-app/src/Components/Content/Content.tsx
+++ my-app/src/Components/Content/Content.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {MyPosts} from "./My Posts/MyPosts";
 import {ProfileInfo} from "./Profile Info/ProfileInfo";
-import {contentPageType, messagesDataType} from "../Redax/redax";
+import {contentPageType, messagesDataType, stateRootType} from "../Redax/redax";
 
 type contentType = {
     content:contentPageType
+    state?: stateRootType
     addPost: (postMessage: string) => void
+    newPostMessage: (newText: string) => void
 }
 
 export function Content(props: contentType) {
@@ -14,7 +16,9 @@ export function Content(props: contentType) {
             <ProfileInfo />
             <MyPosts
                 messages={props.content.messagesData}
+                newMessages={props.content.newPostMessageState}
                 addPost={props.addPost}
+                newPostMessage = {props.newPostMessage}
             />
 
         </div>
