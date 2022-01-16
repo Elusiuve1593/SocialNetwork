9203187d636675f18df7325a4a61cc44e79e74b2
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-01-16 16:13:02 +0200

    Update index.tsx

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {state, stateRootType, messagesDataType, newPostMessage} from './Components/Redax/redax';
-import {addPost} from './Components/Redax/redax';
+import {stateRootType, messagesDataType, store} from './Components/Redax/redax';
 
 export function reRenderEntireTree(state: stateRootType) {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} newPostMessage={newPostMessage}/>
+            <App state={state} dispatch={store.dispatch.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-reRenderEntireTree(state)
+reRenderEntireTree(store.getState())
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
