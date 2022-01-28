9b876153ee8aecab70f34525788cf1d2bdb41541
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-01-28 19:44:32 +0200

    Update MyPosts.tsx

--- my-app/src/Components/Content/My Posts/MyPosts.tsx
+++ my-app/src/Components/Content/My Posts/MyPosts.tsx
@@ -1,25 +1,9 @@
 import React from "react";
 import classes from './MyPost.module.css';
 import {Post} from "./Post/Post";
-import {
-    addMessageActionCreator,
-    addNewPostTypeMessage,
-    addPostActionCreator,
-    addPostType, contentPageType, dialogsPageType,
-    messagesDataType,
-    stateRootType
-} from "../../Redax/redax";
+import {PostsPropsType} from "./MyPostsContainer";
 
-type myPostsType = {
-    //state?: stateRootType
-    newMessages: string
-    messages: messagesDataType[]
-    addPosts: () => void
-    newText: (text: string) => void
-    dispatch: (action: addPostType | addNewPostTypeMessage) => void
-}
-
-export function MyPosts(props: myPostsType) {
+export function MyPosts(props: PostsPropsType) {
     const posts = props.messages.map((i) => <Post key={i.id} message={i.message} likesCount={i.likesCount}/>)
 
     function addPosts() {
