86fad359b9bf55ade0baa70a732291c36e698397
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-01-28 19:44:26 +0200

    Update Content.tsx

--- my-app/src/Components/Content/Content.tsx
+++ my-app/src/Components/Content/Content.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import {MyPosts} from "./My Posts/MyPosts";
 import {ProfileInfo} from "./Profile Info/ProfileInfo";
-import {addNewPostTypeMessage, addPostType, contentPageType, stateRootType, store, storeType} from "../Redax/redax";
 import {MyPostsContainer} from "./My Posts/MyPostsContainer";
 
-type contentType = {
-    store: storeType
-    //content: contentPageType
-    //state?: stateRootType
-    // dispatch: (action: addPostType | addNewPostTypeMessage) => void
-}
-
-export function Content(props: contentType) {
+export function Content() {
     return (
         <div>
             <ProfileInfo/>
-            <MyPostsContainer store={props.store}
-            />
+            <MyPostsContainer />
         </div>
     )
 }
