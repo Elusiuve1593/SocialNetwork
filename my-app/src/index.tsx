741132d247b7d9805baedb1487013c34c32c7b1a
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-01-31 18:54:51 +0200

    Update index.tsx

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -6,18 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from "./Components/Redax/redux-store";
 
-export function reRenderEntireTree() {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-reRenderEntireTree()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
