dfaa594dbf7ad938b159f63cd452a0cf0f2ddde6
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-01-31 18:54:44 +0200

    Update Message.tsx

--- my-app/src/Components/Dialogs/Message/Message.tsx
+++ my-app/src/Components/Dialogs/Message/Message.tsx
@@ -3,10 +3,19 @@ import React from "react";
 
 type messagePropsType = {
     message?: string
+    removePost: (id: string) => void
+    id: string
 }
 
 export function Message(props: messagePropsType) {
+    const callBack = () => {
+        props.removePost(props.id)
+    }
     return (
-        <div className={classes.message}>{props.message}</div>
+        <div>
+            <div className={classes.message}>{props.message}</div>
+            <button onClick={callBack}>Delete</button>
+        </div>
+
     )
 }
