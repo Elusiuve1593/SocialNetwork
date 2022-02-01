a2978fa7fe1069d943d3745af879fa12821c5158
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-02-01 19:06:44 +0200

    Update redux-store.ts

--- my-app/src/Components/Redax/redux-store.ts
+++ my-app/src/Components/Redax/redux-store.ts
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import {postReducer} from "./post_reducer";
 import {dialogsReducer} from "./dialogs_reducer";
+import {usersReducer} from "./users_reducer";
 
 export const rootReducer = combineReducers({
     postReducer,
     dialogsReducer,
+    usersReducer,
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
