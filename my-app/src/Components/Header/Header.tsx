6c62a324fcc353a52aa04bf480b503e5ada95b77
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-02-15 18:47:01 +0200

    Update Header.tsx

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 export function Header() {
     return (
         <div className={classes.header}>
             <header>
-                <img src='https://toppng.com/uploads/preview/design-for-logo-11549988276bxsuzsd1y1.png'/>
+                <NavLink to={'/content'}>
+                    <img src='https://toppng.com/uploads/preview/design-for-logo-11549988276bxsuzsd1y1.png'/>
+                </NavLink>
+
             </header>
         </div>
     )
