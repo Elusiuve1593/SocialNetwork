34341a387e33b56dafcd8e9ccf227ed980ee80df
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-02-16 19:02:56 +0200

    Update UsersContainer.tsx

--- my-app/src/Components/Users/UsersContainer.tsx
+++ my-app/src/Components/Users/UsersContainer.tsx
@@ -1,10 +1,19 @@
 import {connect} from "react-redux";
-import {currentPageAC, followAC, setUsersAC, setUsersTotalCountAC, unfollowAC, userType} from "../Redax/users_reducer";
+import {
+    follow,
+    unFollow,
+    setUsers,
+    setCurrentPage,
+    setPreloader,
+    setTotalUsersCount,
+    userType
+} from "../Redax/users_reducer";
 import {AppStateType} from "../Redax/redux-store";
 import {Dispatch} from "redux";
 import React from "react";
 import axios from "axios";
 import {UsersComponent} from "./UsersComponent";
+import {Preloader} from "../Common/Preloader/Preloader";
 
 
 export type onClickHandlerType = {
@@ -14,6 +23,7 @@ export type onClickHandlerType = {
 class Users extends React.Component<PostsPropsType> {
     componentDidMount() {
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setPreloader?.(false)
             this.props.setUsers(response.data.items)
             this.props.setTotalUsersCount(response.data.totalCount)
         })
@@ -22,23 +32,27 @@ class Users extends React.Component<PostsPropsType> {
     onClickHandler = (i: any) => {
         this.props.setCurrentPage(i)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setPreloader?.(false)
             this.props.setUsers(response.data.items)
         })
     }
 
     render() {
-        return <UsersComponent
-            totalUsersCount={this.props.totalUsersCount}
-            pageSize={this.props.pageSize}
-            users={this.props.users}
-            follow={this.props.follow}
-            unFollow={this.props.unFollow}
-            onClickHandler={this.onClickHandler}
-            currentPage={this.props.currentPage}
-            setCurrentPage={this.props.setCurrentPage}
-            setTotalUsersCount={this.props.setTotalUsersCount}
-            setUsers={this.props.setUsers}/>
-
+        return <>
+            {this.props.isFetching ? <Preloader/> : null}
+            <UsersComponent
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                users={this.props.users}
+                follow={this.props.follow}
+                unFollow={this.props.unFollow}
+                onClickHandler={this.onClickHandler}
+                currentPage={this.props.currentPage}
+                setCurrentPage={this.props.setCurrentPage}
+                setTotalUsersCount={this.props.setTotalUsersCount}
+                setUsers={this.props.setUsers}
+            />
+        </>
     }
 }
 
@@ -49,6 +63,7 @@ export type MapStateToPropsType = {
     pageSize: number
     totalUsersCount: number
     currentPage: number
+    isFetching?: boolean
 }
 export type MapDispatchToPropsType = {
     follow: (id: string | number) => void
@@ -56,6 +71,7 @@ export type MapDispatchToPropsType = {
     setUsers: (users: userType[]) => void
     setCurrentPage: (pageNumber: number) => void
     setTotalUsersCount: (totalCount: number) => void
+    setPreloader?: (setPreloader: boolean) => void
 }
 
 export function MapStateToProps(state: AppStateType): MapStateToPropsType {
@@ -64,28 +80,33 @@ export function MapStateToProps(state: AppStateType): MapStateToPropsType {
         pageSize: state.usersReducer.pageSize,
         totalUsersCount: state.usersReducer.totalCount,
         currentPage: state.usersReducer.currentPage,
+        isFetching: state.usersReducer.isFetching,
     }
 }
 
-export function MapDispatchToProps(dispatch: Dispatch): MapDispatchToPropsType {
-    return {
-        follow: (id: string | number) => {
-            dispatch(followAC(id))
-        },
-        unFollow: (id: string | number) => {
-            dispatch(unfollowAC(id))
-        },
-        setUsers: (users: userType[]) => {
-            dispatch(setUsersAC(users))
-        },
-        setCurrentPage: (pageNumber) => {
-            dispatch(currentPageAC(pageNumber))
-        },
-        setTotalUsersCount: (totalCount) => {
-            dispatch(setUsersTotalCountAC(totalCount))
-        }
+// export function MapDispatchToProps(dispatch: Dispatch): MapDispatchToPropsType {
+//     return {
+//         follow: (id: string | number) => {
+//             dispatch(followAC(id))
+//         },
+//         unFollow: (id: string | number) => {
+//             dispatch(unfollowAC(id))
+//         },
+//         setUsers: (users: userType[]) => {
+//             dispatch(setUsersAC(users))
+//         },
+//         setCurrentPage: (pageNumber) => {
+//             dispatch(currentPageAC(pageNumber))
+//         },
+//         setTotalUsersCount: (totalCount) => {
+//             dispatch(setUsersTotalCountAC(totalCount))
+//         },
+//         setPreloader: (setPreloader: boolean) => {
+//             dispatch(setPreloaderAC(setPreloader))
+//         }
+//     }
+// }
 
-    }
-}
-
-export const UsersContainer = connect(MapStateToProps, MapDispatchToProps)(Users)
+export const UsersContainer = connect(MapStateToProps, {
+    follow, unFollow, setUsers, setCurrentPage, setTotalUsersCount, setPreloader
+})(Users)
