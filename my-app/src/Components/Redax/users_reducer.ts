cc260896451875ff5b530581a23be13d402e05a7
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-02-16 19:02:51 +0200

    Update users_reducer.ts

--- my-app/src/Components/Redax/users_reducer.ts
+++ my-app/src/Components/Redax/users_reducer.ts
@@ -3,6 +3,7 @@ export type usersType = {
     pageSize: number
     totalCount: number
     currentPage: number
+    isFetching: boolean
 }
 
 export type userType = {
@@ -21,9 +22,10 @@ export type initialStatePostsType = usersType
 
 const initialState: usersType = {
     users: [],
-    pageSize: 20,
+    pageSize: 100,
     totalCount: 19,
     currentPage: 1,
+    isFetching: true,
 }
 
 export function usersReducer(state: initialStatePostsType = initialState, action: generalType): initialStatePostsType {
@@ -56,6 +58,12 @@ export function usersReducer(state: initialStatePostsType = initialState, action
                 totalCount: action.payload.totalCount
             }
         }
+        case "SET_PRELOADER": {
+            return{
+                ...state,
+                isFetching: action.payload.setPreloader
+            }
+        }
         default:
             return {...state}
 
@@ -67,43 +75,53 @@ export type generalType = followACType
     | setUsersACType
     | currentPageACType
     | setUsersTotalCountACType
-export type followACType = ReturnType<typeof followAC>
-export type unfollowACType = ReturnType<typeof unfollowAC>
-export type setUsersACType = ReturnType<typeof setUsersAC>
-export type currentPageACType = ReturnType<typeof currentPageAC>
-export type setUsersTotalCountACType = ReturnType<typeof setUsersTotalCountAC>
+    | setPreloaderACType
 
-export function followAC(id: string | number) {
+export type followACType = ReturnType<typeof follow>
+export type unfollowACType = ReturnType<typeof unFollow>
+export type setUsersACType = ReturnType<typeof setUsers>
+export type currentPageACType = ReturnType<typeof setCurrentPage>
+export type setUsersTotalCountACType = ReturnType<typeof setTotalUsersCount>
+export type setPreloaderACType = ReturnType<typeof setPreloader>
+
+export function follow(id: string | number) {
     return {
         type: 'FOLLOW',
         payload: {id}
     } as const
 }
 
-export function unfollowAC(id: string | number) {
+export function unFollow(id: string | number) {
     return {
         type: 'UNFOLLOW',
         payload: {id}
     } as const
 }
 
-export function setUsersAC(users: userType[]) {
+export function setUsers(users: userType[]) {
     return {
         type: 'SET_USERS',
         payload: {users}
     } as const
 }
 
-export function currentPageAC(currentPage: number) {
+export function setCurrentPage(currentPage: number) {
     return {
         type: 'CURRENT_PAGE',
         payload: {currentPage}
     } as const
 }
 
-export function setUsersTotalCountAC(totalCount: number) {
+export function setTotalUsersCount(totalCount: number) {
     return {
         type: 'SET_TOTAL_COUNT',
         payload: {totalCount}
+    } as const
+}
+
+export function setPreloader(setPreloader: boolean) {
+    return {
+        type: 'SET_PRELOADER',
+        payload: {setPreloader}
     } as const
 }
