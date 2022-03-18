5e3e888bfc0f23d34754f3f26cb9a314f3948079
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-03-18 21:39:21 +0200

    Update UsersContainer.tsx

--- my-app/src/Components/Users/UsersContainer.tsx
+++ my-app/src/Components/Users/UsersContainer.tsx
@@ -22,19 +22,21 @@ export type onClickHandlerType = {
 
 class Users extends React.Component<PostsPropsType> {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setPreloader?.(false)
-            this.props.setUsers(response.data.items)
-            this.props.setTotalUsersCount(response.data.totalCount)
-        })
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials: true})
+            .then(response => {
+                this.props.setPreloader?.(false)
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
     }
 
     onClickHandler = (i: any) => {
         this.props.setCurrentPage(i)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setPreloader?.(false)
-            this.props.setUsers(response.data.items)
-        })
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {withCredentials: true})
+            .then(response => {
+                this.props.setPreloader?.(false)
+                this.props.setUsers(response.data.items)
+            })
     }
 
     render() {
