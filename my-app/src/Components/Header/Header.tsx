a0d7e00f0134553003daf67f28f771cd6c1a027f
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-03-18 21:39:07 +0200

    Update Header.tsx

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import classes from './Header.module.css';
 import {NavLink} from "react-router-dom";
+import {mapStateToPropsType} from "./HeaderContainer";
 
-export function Header() {
+export function Header(props: mapStateToPropsType) {
     return (
         <div className={classes.header}>
             <header>
                 <NavLink to={'/content'}>
-                    <img src='https://toppng.com/uploads/preview/design-for-logo-11549988276bxsuzsd1y1.png'/>
+                    <img
+                        src='https://thumbs.dreamstime.com/b/japan-wave-red-sun-logo-japanese-oriental-style-vector-art-illustration-circle-linear-outline-asian-ocean-blue-173623036.jpg'/>
                 </NavLink>
-
+                <div className={classes.login}>
+                    {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+                </div>
             </header>
         </div>
     )
