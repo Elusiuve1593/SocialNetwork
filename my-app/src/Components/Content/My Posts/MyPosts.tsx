6f843996e9e4d68ee94e82f20c10bf132d60323b
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-03-18 21:38:51 +0200

    Update MyPosts.tsx

--- my-app/src/Components/Content/My Posts/MyPosts.tsx
+++ my-app/src/Components/Content/My Posts/MyPosts.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import classes from './MyPost.module.css';
 import {Post} from "./Post/Post";
 import {PostsPropsType} from "./MyPostsContainer";
+import {setDecreaseLike} from "../../Redax/post_reducer";
 
 export function MyPosts(props: PostsPropsType) {
-    const posts = props.messages.map((i) => <Post key={i.id} message={i.message} likesCount={i.likesCount} id={i.id} dispatchType={props.removePost}/>)
+    const posts = props.messages.map((i) => <Post key={i.id} message={i.message} likesCount={i.likesCount} id={i.id}
+                                                  dispatchType={props.removePost} setAddLike={props.setAddLike} setDecreaseLike = {props.setDecreaseLike}/>)
 
     function addPosts() {
         props.addPosts()
@@ -27,7 +29,6 @@ export function MyPosts(props: PostsPropsType) {
                     <textarea ref={newPostElement} value={props.newMessages}
                               onChange={newText}/>
                     <button onClick={addPosts}>Add post</button>
-                    <button>Remove post</button>
                     {posts}
                 </div>
             </div>
