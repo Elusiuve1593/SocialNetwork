9c5018fe33d91a0ac9e2678eba6153397f9c5c8a
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-03-18 21:39:14 +0200

    Update post_reducer.ts

--- my-app/src/Components/Redax/post_reducer.ts
+++ my-app/src/Components/Redax/post_reducer.ts
@@ -1,26 +1,76 @@
 import {v1} from "uuid";
 
+export type postType = {
+    newPostMessageState: string
+    profile: profileType
+    messagesData: messagesDataType[]
+}
+
+export type profileType = {
+    aboutMe: string
+    contacts: {
+        facebook: string
+        website: null | undefined
+        vk: string
+        twitter: string
+        instagram: string
+        youtube: null | undefined
+        github: string
+        mainLink: null | undefined
+    }
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    fullName: string
+    userId: number
+    photos: {
+        small: string
+        large: string
+    }
+}
+
 export type messagesDataType = {
     id: string
     message: string
     likesCount: number
 }
 
-export type initialStatePostsType = typeof initialState
+export type initialStatePostsType = postType
 
-export const initialState = {
+const initialState: postType = {
     newPostMessageState: '',
+    profile: {
+        aboutMe: 'Learning',
+        contacts: {
+            facebook: 'facebook',
+            website: undefined,
+            vk: 'vk',
+            twitter: 'twitter',
+            instagram: 'instagram',
+            youtube: undefined,
+            github: 'github',
+            mainLink: undefined
+        },
+        lookingForAJob: true,
+        lookingForAJobDescription: 'lookingForAJobDescription',
+        fullName: 'fullName',
+        userId: 1,
+        photos: {
+            small: 'small',
+            large: 'large',
+        }
+    },
     messagesData: [
         {
             id: v1(),
             message: 'Hi, how are u?',
-            likesCount: 34,
+            likesCount: 8,
         },
         {
             id: v1(),
             message: 'Crazy people around me...',
             likesCount: 4,
         },
+
     ] as messagesDataType[]
 }
 
@@ -36,28 +86,49 @@ export function postReducer(state: initialStatePostsType = initialState, action:
             // stateCopy.messagesData = [...state.messagesData]
             // stateCopy.messagesData.push(newPost)
             // return stateCopy
-
             return {
                 ...state,
                 newPostMessageState: '',
-                messagesData: [...state.messagesData, {id: v1(), message: state.newPostMessageState, likesCount: 0}]
+                messagesData: [...state.messagesData, {id: v1(), message: state.newPostMessageState, likesCount: 1}]
             }
         case "REMOVE_POST":
             return {
                 ...state, messagesData: [...state.messagesData.filter(i => i.id !== action.id)]
 
             }
+        case "ADD_LIKE": {
+            return {
+                ...state,
+                messagesData: [...state.messagesData.filter(i => i.id === action.id ? i.likesCount++ : i.likesCount)]
+            }
+        }
+        case "DECREASE_LIKE":{
+            return{
+                ...state,
+                messagesData: [...state.messagesData.filter(i => i.id === action.id ? i.likesCount-- : i.likesCount)]
+            }
+        }
         case 'NEW_POST_MESSAGE' :
             return {...state, newPostMessageState: action.newText}
+        case "SET_USERS_PROFILE":
+            return {...state, profile: action.profile}
         default:
             return {...state}
     }
 }
 
-export type generalACType = addPostActionCreatorType | addMessageActionCreator | removePostAC ;
+export type generalACType = addPostActionCreatorType
+    | addMessageActionCreator
+    | removePostAC
+    | setUsersProfile
+    | setAddLike
+    | setDecreaseLike;
 export type addPostActionCreatorType = ReturnType<typeof addPosts>
 export type removePostAC = ReturnType<typeof removePost>
 export type addMessageActionCreator = ReturnType<typeof newText>
+export type setUsersProfile = ReturnType<typeof setUsersProfile>
+export type setAddLike = ReturnType<typeof setAddLike>
+export type setDecreaseLike = ReturnType<typeof setDecreaseLike>
 
 export function addPosts() {
     return {type: 'ADD_POST'} as const
@@ -69,5 +140,23 @@ export function removePost(id: string) {
 
 export function newText(text: string) {
     return {type: 'NEW_POST_MESSAGE', newText: text} as const
+}
+
+export function setUsersProfile(profile: profileType) {
+    return {type: 'SET_USERS_PROFILE', profile} as const
+}
+
+export function setAddLike(id: string) {
+    return {
+        type: 'ADD_LIKE',
+        id
+    } as const
+}
+
+export function setDecreaseLike(id: string){
+    return {
+        type: 'DECREASE_LIKE',
+        id
+    }as const
 }
 
