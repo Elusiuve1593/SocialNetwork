e64fc9852c88a06ab0ba12060e0f38df2167d03b
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-03-18 21:38:59 +0200

    Update Post.tsx

--- my-app/src/Components/Content/My Posts/Post/Post.tsx
+++ my-app/src/Components/Content/My Posts/Post/Post.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './Post.module.css';
 import {DispatchType} from "../MyPostsContainer";
+import {setDecreaseLike} from "../../../Redax/post_reducer";
 
 
 type messagePropsType = {
@@ -8,20 +9,30 @@ type messagePropsType = {
     likesCount: number
     id: string
     dispatchType: (id: string) => void
+    setAddLike: (id: string) => void
+    setDecreaseLike: (id: string) => void
 }
 
 export function Post(props: messagePropsType) {
     const onClickHandler = () => {
         props.dispatchType(props.id)
     }
+    const onAddLike = () => {
+        props.setAddLike(props.id)
+    }
+    const onDecreaseLike = () => {
+        props.setDecreaseLike(props.id)
+    }
     return (
         <div className={classes.content}>
             <div className={classes.item}>
                 <img src='https://cdn3.iconfinder.com/data/icons/cool-avatars-2/190/00-07-2-512.png'/>
                 {props.message}
+                <button onClick={onClickHandler}>Delete</button>
                 <div>
                     <span>{props.likesCount}</span>
-                    <button onClick={onClickHandler}>Delete</button>
+                    <button onClick={onAddLike}>+</button>
+                    <button onClick={onDecreaseLike}>-</button>
                 </div>
             </div>
         </div>
