f7691fe161a5963bd7d601deeca8dd0316cb8f0f
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-03-18 21:38:43 +0200

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,32 +2,34 @@ import './App.css';
 import './Components/Header/Header.module.css';
 import './Components/Content/Content.module.css';
 import './Components/Navbar/Navbar.module.css';
-import {Header} from "./Components/Header/Header";
 import {Navbar} from "./Components/Navbar/Navbar";
-import {Content} from "./Components/Content/Content";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Music} from "./Components/Music/Music";
 import {News} from "./Components/News/News";
 import {Settings} from "./Components/Settings/Settings";
 import {DialogsContainer} from "./Components/Dialogs/DialogsContainer";
 import {UsersContainer} from "./Components/Users/UsersContainer";
+import {ContentContainer} from "./Components/Content/ContentContainer";
+import {BrowserRouter} from "react-router-dom";
+import {Route} from "react-router-dom";
+import {Switch} from "react-router-dom";
+import {HeaderContainer} from "./Components/Header/HeaderContainer";
 
 function App(props: any) {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
-                <Header/>
+                <HeaderContainer/>
                 <Navbar/>
                 <div className={'app-wrapper-content'}>
-                    <Routes>
-                        <Route path='/content/*' element={<Content/>}/>
-                        <Route path='/dialogs/*'
-                               element={<DialogsContainer/>}/>
-                        <Route path='/users/*' element={<UsersContainer />}/>
-                        <Route path='/news/*' element={<News/>}/>
-                        <Route path='/music/*' element={<Music/>}/>
-                        <Route path='/settings/*' element={<Settings/>}/>
-                    </Routes>
+                    <Switch>
+                        <Route path='/content/:userId?' render={() => <ContentContainer/>}/>
+                        <Route path='/dialogs/'
+                               render={() => <DialogsContainer/>}/>
+                        <Route path='/users/' render={() => <UsersContainer/>}/>
+                        <Route path='/news/' render={() => <News/>}/>
+                        <Route path='/music/' render={() => <Music/>}/>
+                        <Route path='/settings/' render={() => <Settings/>}/>
+                    </Switch>
                 </div>
             </div>
         </BrowserRouter>
