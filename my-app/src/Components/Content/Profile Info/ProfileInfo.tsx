0dba28773ea0d3dc8c7079be2b90cbe2e48ddc37
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-03-18 21:39:01 +0200

    Update ProfileInfo.tsx

--- my-app/src/Components/Content/Profile Info/ProfileInfo.tsx
+++ my-app/src/Components/Content/Profile Info/ProfileInfo.tsx
@@ -1,14 +1,45 @@
 import React from "react";
+import {profileType} from "../../Redax/post_reducer";
+import {Preloader} from "../../Common/Preloader/Preloader";
 
-export function ProfileInfo() {
+type ProfileInfoType = {
+    profile: profileType
+}
+// profile: {
+//
+//
+//     lookingForAJob: true,
+//         lookingForAJobDescription: 'lookingForAJobDescription',
+//         fullName: 'fullName',
+//         userId: 1,
+//         photos: {
+//         small: 'small',
+//             large: 'large',
+//     }
+// },
+export function ProfileInfo(props: ProfileInfoType) {
+    if (!props.profile) {
+        return <Preloader/>
+    }
     return (
         <div>
             <div>
                 <img
-                    src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' width='1106px'/>
+                    src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+                    width='1106px'/>
             </div>
             <div>
-                Ava+Dscr
+                <img src={props.profile.photos.large}/>
+                <div><b>fullName:</b> {props.profile.fullName}</div>
+                <div><b>Status: </b>{props.profile.aboutMe}</div>
+                <h3>Contacts:</h3>
+                <ul>
+                    <li>{props.profile.contacts.facebook}</li>
+                    <li>{props.profile.contacts.vk}</li>
+                    <li>{props.profile.contacts.twitter}</li>
+                    <li>{props.profile.contacts.instagram}</li>
+                    <li>{props.profile.contacts.github}</li>
+                </ul>
             </div>
         </div>
     )
