f74a5fb19586494b01eb1e3648ac110945721f1d
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-03-18 21:38:46 +0200

    Update Content.tsx

--- my-app/src/Components/Content/Content.tsx
+++ my-app/src/Components/Content/Content.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import {ProfileInfo} from "./Profile Info/ProfileInfo";
 import {MyPostsContainer} from "./My Posts/MyPostsContainer";
+import {profileType} from "../Redax/post_reducer";
+import {PostsPropsType} from "./ContentContainer";
 
-export function Content() {
+export type ContentType = {
+    profile: profileType
+}
+
+export function Content(props: ContentType) {
     return (
         <div>
-            <ProfileInfo/>
-            <MyPostsContainer />
+            <ProfileInfo profile={props.profile}/>
+            <MyPostsContainer/>
         </div>
     )
 }
