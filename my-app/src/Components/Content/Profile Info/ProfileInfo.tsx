3a9ae9b1378cf84d27ce9a10e03da5a8bfe306c5
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-05-10 10:02:35 +0300

    Update ProfileInfo.tsx

--- my-app/src/Components/Content/Profile Info/ProfileInfo.tsx
+++ my-app/src/Components/Content/Profile Info/ProfileInfo.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import {profileType} from "../../Redax/post_reducer";
 import {Preloader} from "../../Common/Preloader/Preloader";
 import {Redirect} from 'react-router-dom'
+import {ProfileStatus} from "../ProfileStatus/ProfileStatus";
 
 type ProfileInfoType = {
     profile: profileType
-    authReducer: boolean
 }
 
 export function ProfileInfo(props: ProfileInfoType) {
     if (!props.profile) {
         return <Preloader/>
     }
-    if (!props.authReducer) {
-        return <Redirect to={'/login/'}/>
-    }
+    // if (!props.authReducer) {
+    //     return <Redirect to={'/login/'}/>
+    // }
     return (
         <div>
             <div>
@@ -25,7 +25,7 @@ export function ProfileInfo(props: ProfileInfoType) {
             <div>
                 <img src={props.profile.photos.large}/>
                 <div><b>fullName:</b> {props.profile.fullName}</div>
-                <div><b>Status: </b>{props.profile.aboutMe}</div>
+                <div><b>Status: </b> <ProfileStatus aboutMe={props.profile.aboutMe}/></div>
                 <h3>Contacts:</h3>
                 <ul>
                     <li>{props.profile.contacts.facebook}</li>
