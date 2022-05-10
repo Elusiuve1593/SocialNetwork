30ee873d13421d486ec6dddd77b32b2d44d882eb
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-05-10 10:02:30 +0300

    Update Content.tsx

--- my-app/src/Components/Content/Content.tsx
+++ my-app/src/Components/Content/Content.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import {ProfileInfo} from "./Profile Info/ProfileInfo";
 import {MyPostsContainer} from "./My Posts/MyPostsContainer";
 import {profileType} from "../Redax/post_reducer";
-import {PostsPropsType} from "./ContentContainer";
+import {ProfileStatus} from "./ProfileStatus/ProfileStatus";
 
 export type ContentType = {
     profile: profileType
-    authReducer: boolean
 }
 
 export function Content(props: ContentType) {
     return (
         <div>
-            <ProfileInfo profile={props.profile} authReducer={props.authReducer}/>
+            <ProfileInfo profile={props.profile}/>
             <MyPostsContainer/>
         </div>
     )
