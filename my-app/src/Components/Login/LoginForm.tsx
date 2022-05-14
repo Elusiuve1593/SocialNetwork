534ed413663425465d524b3ac683c44136129cf5
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-05-14 17:09:31 +0300

    Create LoginForm.tsx

---
+++ my-app/src/Components/Login/LoginForm.tsx
@@ -0,0 +1,18 @@
+export function LoginForm() {
+    return (
+        <form action="">
+            <div>
+                <input placeholder={"Login"}/>
+            </div>
+            <div>
+                <input placeholder={"Password"}/>
+            </div>
+            <div>
+                <input type={"checkbox"}/> Remember me!
+            </div>
+            <div>
+                <button>Press</button>
+            </div>
+        </form>
+    )
+}
