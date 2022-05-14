7e433f0be72ccbb8c8d743453ca49c49c8b9cb5c
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-05-14 17:09:20 +0300

    Update Content.tsx

--- my-app/src/Components/Content/Content.tsx
+++ my-app/src/Components/Content/Content.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {ProfileInfo} from "./Profile Info/ProfileInfo";
 import {MyPostsContainer} from "./My Posts/MyPostsContainer";
 import {profileType} from "../Redax/post_reducer";
-import {ProfileStatus} from "./ProfileStatus/ProfileStatus";
 
 export type ContentType = {
     profile: profileType
