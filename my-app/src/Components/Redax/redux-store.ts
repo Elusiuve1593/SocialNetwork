8f143b9e46f8d2718edfa1d2f637caea28ad6e6c
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-05-15 14:04:46 +0300

    Update redux-store.ts

--- my-app/src/Components/Redax/redux-store.ts
+++ my-app/src/Components/Redax/redux-store.ts
@@ -4,12 +4,14 @@ import {dialogsReducer} from "./dialogs_reducer";
 import {usersReducer} from "./users_reducer";
 import {authReducer} from "./auth_reducer";
 import thunk from "redux-thunk";
+import {reducer as formReducer} from "redux-form";
 
 export const rootReducer = combineReducers({
     postReducer,
     dialogsReducer,
     usersReducer,
     authReducer,
+    form: formReducer,
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
