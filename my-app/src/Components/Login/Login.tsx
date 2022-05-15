760e9a50a5b3847a4933877bf1e6799da8373df1
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-05-15 14:04:53 +0300

    Update Login.tsx

--- my-app/src/Components/Login/Login.tsx
+++ my-app/src/Components/Login/Login.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import {LoginForm} from "./LoginForm";
+import {FormDataType, LoginReduxForm} from "./LoginForm";
 
-export function Login() {
+export function Login(props: FormDataType) {
+    const onSubmit = () => {
+
+    }
     return (
         <>
             <h1>LOGIN</h1>
-            <LoginForm/>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </>
     )
 }
