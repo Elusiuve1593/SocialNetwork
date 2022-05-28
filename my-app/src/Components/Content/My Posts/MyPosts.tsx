97233dfbc4f9cfd83b77b97e6d9ce453acc16517
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-05-28 18:20:12 +0300

    Update MyPosts.tsx

--- my-app/src/Components/Content/My Posts/MyPosts.tsx
+++ my-app/src/Components/Content/My Posts/MyPosts.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, {memo} from "react";
 import classes from './MyPost.module.css';
 import {Post} from "./Post/Post";
 import {PostsPropsType} from "./MyPostsContainer";
-import {setDecreaseLike} from "../../Redax/post_reducer";
 
-export function MyPosts(props: PostsPropsType) {
+export const MyPosts = memo((props: PostsPropsType) => {
     const posts = props.messages.map((i) => <Post key={i.id} message={i.message} likesCount={i.likesCount} id={i.id}
-                                                  dispatchType={props.removePost} setAddLike={props.setAddLike} setDecreaseLike = {props.setDecreaseLike}/>)
+                                                  dispatchType={props.removePost} setAddLike={props.setAddLike}
+                                                  setDecreaseLike={props.setDecreaseLike}/>)
 
     function addPosts() {
         props.addPosts()
@@ -35,4 +35,4 @@ export function MyPosts(props: PostsPropsType) {
 
         </div>
     )
-}
+})
