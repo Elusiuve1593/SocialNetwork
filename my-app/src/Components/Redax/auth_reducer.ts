e115f03b26983837afeede5df7e662b32c9eeafa
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-06-19 13:56:08 +0300

    Update auth_reducer.ts

--- my-app/src/Components/Redax/auth_reducer.ts
+++ my-app/src/Components/Redax/auth_reducer.ts
@@ -1,3 +1,6 @@
+import {Dispatch} from "redux";
+import {api} from "../Axios/axios";
+
 export type initialStateType = {
     id: number
     email: null | string
@@ -17,8 +20,7 @@ export function authReducer(state: initialStateType = initialState, action: setU
         case "AUTH_REDUCER": {
             return {
                 ...state,
-                ...action.data,
-                isAuth: true,
+                ...action.payload,
             }
         }
         default:
@@ -28,9 +30,46 @@ export function authReducer(state: initialStateType = initialState, action: setU
 
 export type setUserDataType = ReturnType<typeof setUserData>
 
-export function setUserData(data: initialStateType) {
+export function setUserData(userId: number, email: string, login: string, isAuth: boolean) {
     return {
         type: "AUTH_REDUCER",
-        data
+        payload: {userId, email, login, isAuth}
     } as const
+}
+
+export const authThunk = () => {
+    return (dispatch: Dispatch) => {
+        api.authStatus()
+            .then(res => {
+                if (res.data.resultCode === 0) {
+                    const {userId, email, login} = res.data.data
+                    dispatch(setUserData(userId, email, login, true))
+                }
+            })
+    }
+}
+
+export const login = (email: string, password: string, rememberMe: boolean) => {
+    return (dispatch: Dispatch) => {
+        api.login(email, password, rememberMe)
+            .then((res) => {
+                if (res.data.resultCode === 0) {
+                    // @ts-ignore
+                    dispatch(authThunk())
+                }
+            })
+    }
+}
+
+export const logout = () => {
+    return (dispatch: Dispatch) => {
+        api.logout()
+            .then(res => {
+                    if (res.data.resultCode === 0) {
+                        // @ts-ignore
+                        dispatch(setUserData(null, null, null, false))
+                    }
+                }
+            )
+    }
 }
