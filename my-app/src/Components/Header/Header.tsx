b7c657008aa8f212ffae2980e566413c0f4ba965
Elusiuve1593 <93787774+Elusiuve1593@users.noreply.github.com>
2022-06-19 13:55:59 +0300

    Update Header.tsx

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import classes from './Header.module.css';
-import {NavLink} from "react-router-dom";
+import {NavLink, Redirect} from "react-router-dom";
 import {mapStateToPropsType} from "./HeaderContainer";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../Redax/redux-store";
+import {logout} from "../Redax/auth_reducer";
 
 export function Header(props: mapStateToPropsType) {
+    const isAuth = useSelector<AppStateType>(state => state.authReducer.isAuth)
+    const dispatch = useDispatch()
+    const onClickHandler = () => {
+        dispatch(logout())
+    }
     return (
         <div className={classes.header}>
             <header>
@@ -13,6 +21,7 @@ export function Header(props: mapStateToPropsType) {
                 </NavLink>
                 <div className={classes.login}>
                     {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+                    <button className={classes.logout} onClick={onClickHandler}>Logout</button>
                 </div>
             </header>
         </div>
