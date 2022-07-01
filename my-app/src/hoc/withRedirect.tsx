5cdecf362844adf98b5ba5292a19a3e832cfe31f
Elusiuve1593 <elusiuve15.09.93@icloud.com>
2022-07-01 18:01:50 +0300

    Update withRedirect.tsx

--- my-app/src/hoc/withRedirect.tsx
+++ my-app/src/hoc/withRedirect.tsx
@@ -1,4 +1,5 @@
 import React, {ComponentType} from "react";
+// @ts-ignore
 import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import {AppStateType} from "../Components/Redax/redux-store";
